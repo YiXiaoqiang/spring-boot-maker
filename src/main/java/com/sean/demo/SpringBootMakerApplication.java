5b1f2f45a0bd869e1dd756def88f94a4ab0cf8e2
Sean Yi <1498280793@qq.com>
2021-01-25 22:33:12 +0800

    init spring boot

---
+++ src/main/java/com/sean/demo/SpringBootMakerApplication.java
@@ -0,0 +1,13 @@
+package com.sean.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMakerApplication.class, args);
+	}
+
+}
